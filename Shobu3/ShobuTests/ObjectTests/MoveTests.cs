b64967115c1f39fd04bb9c733e5e2bf374bcbf38
Jordan <jordannelsen3000@gmail.com>
2020-10-29 14:10:48 -0400

    adjusted repo location on local machine, finished logic tests, started object tests

--- Shobu3/ShobuTests/ObjectTests/MoveTests.cs
+++ Shobu3/ShobuTests/ObjectTests/MoveTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using Shobu3.GameLogic;
 using Shobu3.Objects;
+using Shobu3;
 
 namespace ShobuTests.ObjectTests
 {
@@ -11,9 +12,18 @@ namespace ShobuTests.ObjectTests
     public class MoveTests
     {
         [TestMethod]
-        public void TransitionSquareIndexTest()
+        [DataRow(-1,4)]
+        [DataRow(4,8)]
+        [DataRow(5,10)]
+        public void TransitionSquareIndexTest(int expected, int endIndex)
         {
-
+            // Arrange
+            Board testBoard = new Board(1);
+            Move testMove = new Move(testBoard.SquaresOnBoard[0], testBoard.SquaresOnBoard[endIndex], testBoard, PlayerName.X, true);
+            // Act
+            int result = testMove.TransitionSquareIndex();
+            // Assert
+            Assert.AreEqual(expected, result);
         }
     }
 }
